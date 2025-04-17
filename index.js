748b58005392f9404c9d330c8cebf2c483aa7136
yl1243 <gloria19984089@gmail.com>
2025-04-16 19:48:13 -0700

    echo

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ import "dotenv/config";
 import session from "express-session";
 
 const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
+
 mongoose.connect(CONNECTION_STRING);
 
 const app = express()
